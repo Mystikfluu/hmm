22dcd4865c65c0175a4eb01a858e0e7715872f0c
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2020-08-09 05:30:41 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 _G.toget = "https://raw.githubusercontent.com/Mystikfluu/clicking_masters/master/oldstyle.lua"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Mystikfluu/hmm/master/loader.lua"))
+repeat wait() until _G.loaded == true
   _G.script = ""
- for i = 0,string.len(_G.obfscript) do
+ for i = 0,string.len(tostring(_G.obfscript)) do
    _G.script = _G.script .. _G.obfscript:split("")[i]
  end
  loadstring(_G.script)
