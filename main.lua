43c6e168b5037b59b089c3dec2e087ce2f35294f
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2020-08-09 05:34:23 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Mystikfluu/hmm/master
 repeat wait() until _G.loaded == true
   _G.script = ""
  for i = 0,string.len(tostring(_G.obfscript)) do
+    spawn(function()
+     print(i)
    _G.script = _G.script .. _G.obfscript:split("")[i]
+      end)
  end
  loadstring(_G.script)
